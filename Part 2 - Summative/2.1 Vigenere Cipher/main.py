3fc6f56307952f4cf87c186fc5be32154ac08e0f
Maggie <2025soper.margaret@fcboe.org>
2024-09-10 11:06:46 -0400

    started 2.1 - encode works still working on decode - eoc, 9/10

--- Part 2 - Summative/2.1 Vigenere Cipher/main.py
+++ Part 2 - Summative/2.1 Vigenere Cipher/main.py
@@ -3,11 +3,35 @@
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def vig_encode(text, keyword):
-  return ""
+  temp = ""
+  num = 0
+  for i in range(len(text)):
+    letter = text[i]
+    if letter in alpha:
+      var = alpha.index(letter) + alpha.index(keyword[num])
+      if var > 25:
+        var = (var - 26) % var
+      temp += alpha[var]
+      num += 1
+      if num >= len(keyword):
+        num = 0
+  return temp
 
 
 def vig_decode(text, keyword):
-  return ""
+  temp = ""
+  num = 3
+  for i in range(len(text)):
+    letter = text[i]
+    if letter in alpha:
+      var = alpha.index(letter) - alpha.index(keyword[num])
+      if len(alpha) < var:
+        var = (var + 26) % var
+      temp += alpha[var]
+      num -= 1
+      if num <= 0:
+        num = 3
+  return temp
 
 
 test = "THEQUICKBROWNFOXJUMPEDOVERTHELAZYDOG"
